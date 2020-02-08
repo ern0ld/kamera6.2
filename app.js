7bbd1a701e332a8cdcb98cfbeda67fe3dbbe7a4a
ern0ld <50329110+ern0ld@users.noreply.github.com>
2020-02-08 09:45:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
+//kameran käsittelyyn tarvittavat alkuarvot muuttujaan
 var alkuarvot = {video: {facingMode: "user"}, audio: false};
-var track = null;
 
-var kamerakuva = document.getElementById("kamerakuva");
-var kuvasijainti = document.getElementById("kuvasijainti");
-var kameracanvas = document.getElementById("kameracanvas");
-var laukaisin = document.getElementById("laukaisin");
+//html-elementeistä vakiot
+const kamerakuva = document.getElementById("kamerakuva"),
+ kuvasijainti = document.getElementById("kuvasijainti"),
+ kameracanvas = document.getElementById("kameracanvas"),
+ laukaisin = document.getElementById("laukaisin");
 
+//Käynnistetään kamera mikäli sellainen on saatavilla
 function cameraStarttaa(){
     navigator.mediaDevices.getUserMedia(alkuarvot)
     .then(function(kuvavirta){ kamerakuva.srcObject = kuvavirta; })
     .catch(function(error){console.log("huppista")})
 }
-
+//näppäimen tapahtumankäsittelijä. Voitaisiin käyttää myös addEventListeneriä
 laukaisin.onclick =  function(){
     kameracanvas.width= kamerakuva.videoWidth;
     kameracanvas.height= kamerakuva.videoHeight;
     kameracanvas.getContext("2d").drawImage(kamerakuva, 0,0);
     kuvasijainti.src = kameracanvas.toDataURL("image/png");
     kameracanvas.hidden = true;
-   // kuvasijainti.classList.add("kuvattu");
-  // kuvasijainti.id = "otto";
-  kuvasijainti.style.width = "100px";
-  kuvasijainti.style.height = "100px"
-  console.log(kuvasijainti.style.width);
+    kuvasijainti.classList.add("kuvattu");
 
 }
-
+//Kuuntelija, kun sisältö ladattu, kutsutaan funktio
 window.addEventListener("load", cameraStarttaa);
 
